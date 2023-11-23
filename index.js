bbc3548b89add912c38b06af8d2b97b5878ff303
khacbac <hokhacbac@gmail.com>
2023-11-23 15:21:21 +0700

    switch to use yarn

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const express = require("express");
-const mongoose = require("mongoose");
-
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log(`Server Started at ${3000}`);
+app.listen(3030, () => {
+  console.log(`Server Started at ${3030}`);
 });
